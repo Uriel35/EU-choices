c4c036b6a22da61e15fea2e1a325992660614d3c
Uriel35 <uriespada@gmail.com>
2024-06-25 20:33:16 -0300

    Quiz

--- js/modal_script.js
+++ js/modal_script.js
@@ -31,8 +31,11 @@ defineModal(undefined, document.getElementById('error-modal'), document.getEleme
 defineModal(document.getElementById('show-circle-modal-button'), document.getElementById('circle-modal'), document.getElementById('circles-modal-ctn'), document.getElementById('close-circles-modal'))
 defineModal(document.getElementById('report-question-button'), document.getElementById('report-question-modal'), document.getElementById('report-question-modal-ctn'), document.getElementById('close-report-question-modal'))
 
+defineModal(document.getElementById('open-quiz-modal'), document.getElementById('quiz-modal'), document.getElementById('quiz-modal-ctn'), document.getElementById('close-quiz-modal'))
 
-
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("quiz-modal").classList.add("flex-active")
+});
 
 //  *************** DATA ANALYTICS EVENTS *****************
 function addAnaltyticEvent(tgt, name) {
